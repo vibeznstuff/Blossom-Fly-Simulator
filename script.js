93eb67dae8dc12597747dacfbc1a04ca15dba677
vibeznstuff <richard.dansoh@gmail.com>
2014-07-25 14:16:30 -0400

    Added Attack and Extra Moves

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-var img_width = 50;
-var img_height = 50;
+var img_width = 43;
+var img_height = 58;
+
+function setDim(width,height){
+    img_width=width;
+    img_height=height;
+}
 
 var img = document.createElement("IMG");
 img.setAttribute("src","images/neutral-right.gif");
@@ -7,11 +12,19 @@ var flying = false;
 var moving = false;
 var right = true;
 var left = false;
+var attacking = false;
 var dist = 0;
 
+var fire_img = document.createElement("IMG");
+fire_img.setAttribute("src","images/fireball-1-right.gif");
+var fireball_speed = 600;
+var fireball = new Projectile(0,0,fireball_speed,fire_img);
+var fireball_exists = false;
+var fireball_time = 54;
+var proj_dist=0;
+
 function setRightRun(){
-    img_height = 50;
-    img_width = 50;
+    setDim(43,58);
     if(left){
             img.setAttribute("src","images/neutral-right.gif");
             dist=0;
@@ -35,8 +48,7 @@ function setRightRun(){
 };
 
 function setLeftRun(){
-    img_height = 50;
-    img_width = 50;
+    setDim(43,58);
     if(right){
             img.setAttribute("src","images/neutral-left.gif");
             dist=0;
@@ -60,8 +72,7 @@ function setLeftRun(){
 };
 
 function setRightTakeOff(){
-    img_height = 50;
-    img_width = 50;
+    setDim(43,58);
     if(left){
             img.setAttribute("src","images/neutral-left.gif");
             dist=0;
@@ -71,23 +82,19 @@ function setRightTakeOff(){
             dist += 1;
             if(dist === 1){
                 img.setAttribute("src","images/takeoff-1-right.gif");
-                img_height = 50;
-                img_width = 50;
+                setDim(43,58);
             } else if (dist === 2){
                 img.setAttribute("src","images/takeoff-2-right.gif");
-                img_height = 50;
-                img_width = 50;
+                setDim(43,58);
             } else if (dist >= 3){
                 img.setAttribute("src","images/fly-right.gif");
-                img_height = 30;
-                img_width = 75;
+                setDim(75,35);
             }
         };
 };
 
 function setLeftTakeOff(){
-    img_height = 50;
-    img_width = 50;
+    setDim(43,58);
     if(right){
             img.setAttribute("src","images/neutral-left.gif");
             dist=0;
@@ -97,18 +104,63 @@ function setLeftTakeOff(){
             dist += 1;
             if(dist === 1){
                 img.setAttribute("src","images/takeoff-1-left.gif");
-                img_height = 50;
-                img_width = 50;
+                setDim(43,58);
             } else if (dist === 2){
                 img.setAttribute("src","images/takeoff-2-left.gif");
-                img_height = 50;
-                img_width = 50;
+                setDim(43,58);
             } else if (dist >= 3){
                 img.setAttribute("src","images/fly-left.gif");
-                img_height = 30;
-                img_width = 75;
+                setDim(75,35);
             }
         };
+};
+
+function setFireBall(){
+    attacking=true;
+    if(right){
+        dist+=1;
+        if(dist===1){
+            img.setAttribute("src","images/attack-1-right.gif");
+            setDim(45,56);
+        } else if (dist===2){
+            img.setAttribute("src","images/attack-2-right.gif");
+            setDim(36,59);
+        } else if (dist===3){
+            img.setAttribute("src","images/attack-3-right.gif");
+            setDim(56,59);
+        } else if (dist===4){
+            img.setAttribute("src","images/attack-4-right.gif");
+        } else if (dist===5){
+            img.setAttribute("src","images/attack-5-right.gif");
+            setDim(53,60);
+        } else if (dist===6){
+            fireball.setImage("images/fireball-1-right.gif");
+            fireball_exists=true;
+            fireball.setPosition(particle.x+img_width*(.8),particle.y+8);
+            fireball.setRight();
+        }
+    } else {
+        dist+=1;
+        if(dist===1){
+            img.setAttribute("src","images/attack-1-left.gif");
+            setDim(45,56);
+        } else if (dist===2){
+            img.setAttribute("src","images/attack-2-left.gif");
+            setDim(36,59);
+        } else if (dist===3){
+            img.setAttribute("src","images/attack-3-left.gif");
+            setDim(56,59);
+        } else if (dist===4){
+            img.setAttribute("src","images/attack-4-left.gif");
+        } else if (dist===5){
+            img.setAttribute("src","images/attack-5-left.gif");
+            setDim(53,60);
+        } else if (dist===6){
+            fireball_exists=true;
+            fireball.setPosition(particle.x-img_width*(.8),particle.y+8);
+            fireball.setLeft();
+        }
+    }
 };
 
 var keys = []; // Or you could call it "key"
@@ -117,44 +169,40 @@ onkeydown = onkeyup = function(e){
     e = e || event; // to deal with IE
     keys[e.keyCode] = e.type == 'keydown';
     /*insert conditional here*/
-    if(keys[37] && keys[38]){ // left and up
+    if(keys[37] && keys[38] && !attacking){ // left and up
         particle.vx += -20;
         particle.vy += -20;
         if(!flying){
             setLeftTakeOff();
         } else {
             img.setAttribute("src","images/fly-left.gif");
-            img_height = 30;
-            img_width = 75;
+            setDim(75,35);
         };
         
         moving = true;
-    } else if (keys[37] && keys[40]){ // left and down
+    } else if (keys[37] && keys[40] && !attacking){ // left and down
         particle.vx += -20;
         particle.vy += 20
         img.setAttribute("src","images/neutral-left.gif");
-        img_height = 50;
-        img_width = 50;
+        setDim(43,58);
         moving = true;
-    } else if (keys[39] && keys[38]){ // right and up
+    } else if (keys[39] && keys[38] && !attacking){ // right and up
         particle.vx += 20;
         particle.vy += -20;
         if(!flying){
             setRightTakeOff();
         } else {
             img.setAttribute("src","images/fly-right.gif");
-            img_height = 30;
-            img_width = 75;
+            setDim(75,35);
         };
         moving = true;
-    } else if (keys[39] && keys[40]){ // right and down
+    } else if (keys[39] && keys[40] && !attacking){ // right and down
         particle.vx += 20;
         particle.vy += 20;
         img.setAttribute("src","images/neutral-right.gif");
-        img_height = 50;
-        img_width = 50;
+        setDim(43,58);
         moving = true;
-    } else if (keys[37]){ // left
+    } else if (keys[37] && !attacking){ // left
         particle.vx += -20;
         if(!flying){
             setLeftRun();
@@ -162,23 +210,20 @@ onkeydown = onkeyup = function(e){
             right = false;
             left = true;
             img.setAttribute("src","images/fly-neutral-left.gif");
-            img_height = 65;
-            img_width = 50;
+            setDim(46,75);
         }
         moving = true;
-    } else if (keys[38]){ // up
+    } else if (keys[38] && !attacking){ // up
         particle.vy += -20;
         if(right){
             img.setAttribute("src","images/fly-neutral-right.gif");
-            img_height = 65;
-            img_width = 50;
+            setDim(46,75);
         } else {
             img.setAttribute("src","images/fly-neutral-left.gif");
-            img_height = 65;
-            img_width = 50;
+            setDim(46,75);
         };
         moving = true;
-    } else if (keys[39]){ // right
+    } else if (keys[39] && !attacking){ // right
         particle.vx += 20;
         if(!flying){
             setRightRun();
@@ -186,34 +231,53 @@ onkeydown = onkeyup = function(e){
             right = true;
             left = false;
             img.setAttribute("src","images/fly-neutral-right.gif");
-            img_height = 65;
-            img_width = 50;
+            setDim(46,75);
         }
         moving = true;
-    } else if (keys[40]){ // down
+    } else if (keys[40] && !attacking){ // down
         particle.vy += 20
-        moving = true;
+        if(!flying){
+            setDim(50,54);
+            moving=false;
+            if(right){
+                img.setAttribute("src","images/crouch-right.gif");
+            } else {
+                img.setAttribute("src","images/crouch-left.gif");
+            };
+        } else {
+            moving = true;
+        };
+    } else if (keys[32]){
+        if(!attacking){
+            dist=0;
+        };
+        setFireBall();
     } else {
         moving = false;
+        attacking = false;
         if(flying){
             if(right && particle.vy >= 0){
                 img.setAttribute("src","images/falling-right.gif");
-                img_height = 50;
-                img_width = 50;
+                setDim(43,59);
             } else if(right && particle.vy < 0){
                 img.setAttribute("src","images/fly-neutral-right.gif");
-                img_height = 65;
-                img_width = 50;
+                setDim(46,75);
             } else if(left && particle.vy >= 0){
                 img.setAttribute("src","images/falling-left.gif");
-                img_height = 50;
-                img_width = 50;
+                setDim(43,59);
             } else if(left && particle.vy < 0){
                 img.setAttribute("src","images/fly-neutral-left.gif");
-                img_height = 65;
-                img_width = 50;
+                setDim(46,75);
             }
-        }; 
+        } else {
+            if(right){
+                img.setAttribute("src","images/neutral-right.gif");
+                setDim(43,58);
+            } else {
+                img.setAttribute("src","images/neutral-left.gif");
+                setDim(43,58);
+            };
+        };
     };  
 };
 
@@ -224,7 +288,82 @@ var ctx = canvas.getContext( "2d" );
 
 
 // Frames-per-second
-var FPS = 100;
+var FPS = 150;
+
+//Projectile object
+function Projectile(x,y,v,img){
+    this.x = x;
+    this.y = y;
+    this.velocity = v;
+    this.img = img;
+    this.draw = function(){
+        var img = fire_img;
+        if(right){
+            img.setAttribute("src", "images/fireball-1-right.gif");
+            ctx.drawImage(img,this.x,this.y,56,43);
+        } else {
+            img.setAttribute("src", "images/fireball-1-left.gif");
+            ctx.drawImage(img,this.x,this.y,56,43);
+        };
+        /*if(right){
+            proj_dist += 1;
+            if(proj_dist === 1){
+                fireball.setImage("images/fireball-1-right.gif");
+                fireball.draw();
+            } else if (proj_dist === 2){
+                fireball.setImage("images/fireball-2-right.gif");
+                fireball.draw();
+            } else if (proj_dist === 3){
+                fireball.setImage("images/fireball-3-right.gif");
+                fireball.draw();
+                proj_dist=0;
+                fireball_time -= 3;
+            };
+            if(proj_dist===fireball_time){
+                fireball_time=54;
+                fireball_exists=false;
+            };
+        } else {
+            proj_dist += 1;
+            if(proj_dist === 1){
+                fireball.setImage("images/fireball-1-left.gif");
+                fireball.draw();
+            } else if (proj_dist === 2){
+                fireball.setImage("images/fireball-2-left.gif");
+                fireball.draw();
+            } else if (proj_dist === 3){
+                fireball.setImage("images/fireball-3-left.gif");
+                fireball.draw();
+                proj_dist=0;
+                fireball_time -= 3;
+            };
+            if(proj_dist===fireball_time){
+                fireball_time=54;
+                fireball_exists=false;
+            };
+        }; */
+    }
+    this.update = function(){
+        this.x += this.velocity / FPS;
+    };
+    this.setImage = function(src){
+        this.img.setAttribute("src",src);
+    }
+    this.setPosition = function(x,y){
+        this.x = x;
+        this.y = y;
+    }
+    this.setRight = function(){
+        if(this.velocity < 0){
+            this.velocity = -this.velocity;
+        };
+    };
+    this.setLeft = function(){
+        if(this.velocity> 0){
+            this.velocity = -this.velocity;
+        };
+    }
+};
 
 // Particle object
 var particle = {
@@ -239,45 +378,39 @@ var particle = {
         ctx.drawImage(img,this.x,this.y,img_width,img_height);
     },
     update: function() {
-        this.vx += this.ax / FPS;
-        this.vy += this.ay / FPS;
-        this.x += this.vx / FPS;
-        this.y += this.vy / FPS;
+        
+        if(!attacking){
+            this.vx += this.ax / FPS;
+            this.vy += this.ay / FPS;
+            this.x += this.vx / FPS;
+            this.y += this.vy / FPS;
+        };
         
         if(this.y < (canvas.height - img_height*1.5)){
             flying = true;
         } else {
             flying = false;
-            if(!moving){
-                if(right){
-                    img.setAttribute("src","images/neutral-right.gif");
-                    img_height = 50;
-                    img_width = 50;
-                } else {
-                    img.setAttribute("src","images/neutral-left.gif");
-                    img_height = 50;
-                    img_width = 50;
-                };
-            };
-        }
+        };
+        
+        
         // Collision detection
         if ( this.x  < -img_width) {
             this.x = canvas.width;
             this.vx = this.vx/2;
-        }
+        };
         if ( this.x > canvas.width) {
             this.x = -img_width;
             this.vx = this.vx/2;
-        }
+        };
         if ( this.y <= 0 ) {
             this.y = 0;
             this.vy = 0;
-        }
+        };
         if ( this.y >= canvas.height - img_height ) {
             this.y = canvas.height - img_height;
             this.vy = -this.vy/6;
             this.vx = this.vx/1.01;
-        }
+        };
     }
 };
 
@@ -285,14 +418,21 @@ var particle = {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     particle.draw();
+    if(fireball_exists){
+        fireball.draw();
+    }
 }
 
 // Game loop update function
 function update() {
     particle.update();
+    if(fireball_exists){
+        fireball.update();
+    }
 }
 
 function tick() {
+    //console.log(fireball.x);
     draw();
     update();
 }
